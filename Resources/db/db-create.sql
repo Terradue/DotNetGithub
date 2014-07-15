272614c5942906c3a14fc70a804162f98c5b26c2
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-07-15 16:10:30 +0000

    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.Github/trunk@37024 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- Resources/db/db-create.sql
+++ Resources/db/db-create.sql
@@ -4,8 +4,21 @@ USE $MAIN$;
 
 /*****************************************************************************/
 
+-- Creating usr_github table
+CREATE TABLE usr_github (
+    id int unsigned NOT NULL auto_increment,
+	id_usr int unsigned NOT NULL COMMENT 'FK: User',
+    username varchar(50) COMMENT 'Username on github',
+	token varchar(50) COMMENT 'Token to access github',
+    CONSTRAINT pk_usrgithub PRIMARY KEY (id_usr),
+    CONSTRAINT fk_usrgithub_usr FOREIGN KEY (id_usr) REFERENCES usr(id) ON DELETE CASCADE
+) Engine=InnoDB COMMENT 'User github';
+-- RESULT
+
+/*****************************************************************************/
+
 -- Adding keys, token and pwd
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('Github-client-id', 'string', 'Github client Id', 'Enter the value of the Github client Id', 'e9d253230620c913d897', '0');
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('Github-client-secret', 'string', 'Github client secret', 'Enter the value of the Github client secret', '763201404f5fca0ad5f9fef558eec94bee2a28c6', '0');
-INSERT INTO config (`name`, `type`, `caption`, `hint`, `value`, `optional`) VALUES ('Github-SSHkey-sandbox', 'string', 'Github ssh key', 'Enter the value of the Github ssh key', 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvVT6BtbzDUKiipcmpEyyvanXyLJ1WGUy5VWVlG7LR8owQ8GTE3Ct5fljb', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `optional`) VALUES ('Github-client-name', 'string', 'Github client name', 'Enter the value of the Github client name', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `optional`) VALUES ('Github-client-id', 'string', 'Github client Id', 'Enter the value of the Github client Id', '0');
+INSERT INTO config (`name`, `type`, `caption`, `hint`, `optional`) VALUES ('Github-client-secret', 'string', 'Github client secret', 'Enter the value of the Github client secret', '0');
 -- RESULT
