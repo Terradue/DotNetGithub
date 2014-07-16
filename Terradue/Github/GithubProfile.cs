cc88f0edfcf7d11e9208f9e32801bb9cd05caa9a
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-07-16 15:22:48 +0000

    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.Github/trunk@37067 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- Terradue/Github/GithubProfile.cs
+++ Terradue/Github/GithubProfile.cs
@@ -10,7 +10,30 @@ namespace Terradue.Github {
     [EntityTable("usr_github", EntityTableConfiguration.Custom)]
     public class GithubProfile : Entity {
 
-        public GithubClient Client { get; set; } 
+        public new int Id { 
+            get { 
+                return base.Id;
+            } 
+            set { 
+                base.Id = value;
+            } 
+        }
+
+        private GithubClient client { get; set; }
+        public GithubClient Client { 
+            get{ 
+                if (client == null) {
+                    client = new GithubClient(context.BaseUrl, 
+                                              context.GetConfigValue("Github-client-name"), 
+                                              context.GetConfigValue("Github-client-id"), 
+                                              context.GetConfigValue("Github-client-secret"));
+                }
+                return client;
+            }
+            set{ 
+                client = value;
+            }
+        } 
 
         [EntityDataField("username")]
         public string GithubName { get; set; }
@@ -20,21 +43,12 @@ namespace Terradue.Github {
 
         public bool HasSSHKey { get; set; }
 
-        public GithubProfile(IfyContext context) : base(context) {
-            this.Client = new GithubClient(context.BaseUrl, context.GetConfigValue("Github-client-name"), context.GetConfigValue("Github-client-id"), context.GetConfigValue("Github-client-secret"));
-        }
-
-        public GithubProfile(IfyContext context, int usrid) : this(context) {
+        public GithubProfile(IfyContext context, int usrid) : base(context) {
             this.Id = usrid;
         }
 
-        public GithubProfile(IfyContext context, GithubClient Application) : base(context) {
-            this.Client = Application;
-        }
-
         public static GithubProfile FromId(IfyContext context, int userId){
-            GithubProfile result = new GithubProfile(context);
-            result.Id = userId;
+            GithubProfile result = new GithubProfile(context, userId);
             result.Load();
             return result;
         }
