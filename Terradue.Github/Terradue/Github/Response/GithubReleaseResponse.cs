fa8eb632151c43121488aad8892024e158250aea
eboissier <eboissier@e07a6ad9-7c28-0410-a8f0-b6e01509c4f6>
2014-10-24 08:02:50 +0000

    git-svn-id: https://gateway.terradue.com/repo/products/sugar/Terradue.Github/trunk@38913 e07a6ad9-7c28-0410-a8f0-b6e01509c4f6

--- Terradue.Github/Terradue/Github/Response/GithubReleaseResponse.cs
+++ Terradue.Github/Terradue/Github/Response/GithubReleaseResponse.cs
@@ -16,6 +16,26 @@ namespace Terradue.Github.Reponse {
         public GithubUserResponse author { get; set; }
         [DataMember]
         public bool prerelease { get; set; }
+        [DataMember]
+        public GithubAssets assets { get; set; }
+    }
+
+    [DataContract]
+    public class GithubAssets {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string label { get; set; }
+        [DataMember]
+        public string url { get; set; }
+        [DataMember]
+        public string content_type { get; set; }
+        [DataMember]
+        public string browser_download_url { get; set; }
+        [DataMember]
+        public long size { get; set; }
     }
 
 
